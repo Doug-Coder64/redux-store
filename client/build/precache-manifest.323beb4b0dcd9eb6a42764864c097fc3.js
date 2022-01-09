0c79cfb08822a83c955f4a1d171087bc946256af
doug peterson <dougpeterson@dougs-MacBook-Pro.local>
2022-01-09 09:32:42 -0600

    heroku deploy

---
+++ client/build/precache-manifest.323beb4b0dcd9eb6a42764864c097fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f88885b0535fe9535c81e47570fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a558b0c818c5a4250292",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "53cd62350967ad025c29",
+    "url": "/static/js/2.4076303c.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.4076303c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a558b0c818c5a4250292",
+    "url": "/static/js/main.9e57c737.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
